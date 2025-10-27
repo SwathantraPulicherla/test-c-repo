8e47e5455a831aff9630c76bd41ebd1b4d1847b1
SwathantraPulicherla <swathantra.pulicherla@requisimus.com>
2025-10-27 11:09:59 +0530

    Add .gitignore and clean up build artifacts
    - Add comprehensive .gitignore for build artifacts, coverage reports, and temp files
    - Remove build directory from git tracking (artifacts should not be committed)

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/SwathantraPulicherla/OneDrive - requisimus Holding GmbH/Desktop/AI_tools_project/workspaces/demo/test-repo
-# Build directory: C:/Users/SwathantraPulicherla/OneDrive - requisimus Holding GmbH/Desktop/AI_tools_project/workspaces/demo/test-repo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
